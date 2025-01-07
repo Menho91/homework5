f436564a2e5241f8439d40f37cf9a89a028dc120
Menho <82595532+Menho91@users.noreply.github.com>
2025-01-07 12:16:25 +0900

    초기 커밋.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/homework/UHT/homework.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodehomework_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_homework;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_homework()
+	{
+		if (!Z_Registration_Info_UPackage__Script_homework.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/homework",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x16E5ECD2,
+				0x384484A1,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_homework.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_homework.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_homework(Z_Construct_UPackage__Script_homework, TEXT("/Script/homework"), Z_Registration_Info_UPackage__Script_homework, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x16E5ECD2, 0x384484A1));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
